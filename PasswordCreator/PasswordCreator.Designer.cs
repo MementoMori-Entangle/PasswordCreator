972d9d611e1dd324e97a50abf1683dfb7b63495f
Entangle <vm.game.jp.002@gmail.com>
2025-05-11 03:04:43 +0900

    プロジェクト ファイルを追加します。

---
+++ PasswordCreator/PasswordCreator.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace PasswordCreator
+{
+    partial class PasswordCreator
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.CRandomButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.NumberCheckBox = new System.Windows.Forms.CheckBox();
+            this.AlphabetUPCheckBox = new System.Windows.Forms.CheckBox();
+            this.AlphabetLOWCheckBox = new System.Windows.Forms.CheckBox();
+            this.SymbolCheckBox = new System.Windows.Forms.CheckBox();
+            this.PasswordRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.AlphabetUPTextBox = new System.Windows.Forms.TextBox();
+            this.AlphabetLOWTextBox = new System.Windows.Forms.TextBox();
+            this.SymbolTextBox = new System.Windows.Forms.TextBox();
+            this.NumberDataTextBox = new System.Windows.Forms.TextBox();
+            this.AlphabetUPDataTextBox = new System.Windows.Forms.TextBox();
+            this.AlphabetLOWDataTextBox = new System.Windows.Forms.TextBox();
+            this.SymbolDataTextBox = new System.Windows.Forms.TextBox();
+            this.InitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.PasswordLabel.Location = new System.Drawing.Point(8, 136);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(86, 19);
+            this.PasswordLabel.TabIndex = 0;
+            this.PasswordLabel.Text = "Password";
+            // 
+            // CRandomButton
+            // 
+            this.CRandomButton.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CRandomButton.Location = new System.Drawing.Point(12, 11);
+            this.CRandomButton.Name = "CRandomButton";
+            this.CRandomButton.Size = new System.Drawing.Size(105, 34);
+            this.CRandomButton.TabIndex = 1;
+            this.CRandomButton.Text = "C Random";
+            this.CRandomButton.UseVisualStyleBackColor = true;
+            this.CRandomButton.Click += new System.EventHandler(this.CRandomButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ClearButton.Location = new System.Drawing.Point(123, 11);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(60, 34);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // NumberCheckBox
+            // 
+            this.NumberCheckBox.AutoSize = true;
+            this.NumberCheckBox.Location = new System.Drawing.Point(12, 51);
+            this.NumberCheckBox.Name = "NumberCheckBox";
+            this.NumberCheckBox.Size = new System.Drawing.Size(63, 16);
+            this.NumberCheckBox.TabIndex = 4;
+            this.NumberCheckBox.Text = "Number";
+            this.NumberCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AlphabetUPCheckBox
+            // 
+            this.AlphabetUPCheckBox.AutoSize = true;
+            this.AlphabetUPCheckBox.Location = new System.Drawing.Point(12, 73);
+            this.AlphabetUPCheckBox.Name = "AlphabetUPCheckBox";
+            this.AlphabetUPCheckBox.Size = new System.Drawing.Size(90, 16);
+            this.AlphabetUPCheckBox.TabIndex = 7;
+            this.AlphabetUPCheckBox.Text = "Alphabet-UP";
+            this.AlphabetUPCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AlphabetLOWCheckBox
+            // 
+            this.AlphabetLOWCheckBox.AutoSize = true;
+            this.AlphabetLOWCheckBox.Location = new System.Drawing.Point(12, 95);
+            this.AlphabetLOWCheckBox.Name = "AlphabetLOWCheckBox";
+            this.AlphabetLOWCheckBox.Size = new System.Drawing.Size(98, 16);
+            this.AlphabetLOWCheckBox.TabIndex = 10;
+            this.AlphabetLOWCheckBox.Text = "Alphabet-LOW";
+            this.AlphabetLOWCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SymbolCheckBox
+            // 
+            this.SymbolCheckBox.AutoSize = true;
+            this.SymbolCheckBox.Location = new System.Drawing.Point(12, 117);
+            this.SymbolCheckBox.Name = "SymbolCheckBox";
+            this.SymbolCheckBox.Size = new System.Drawing.Size(61, 16);
+            this.SymbolCheckBox.TabIndex = 13;
+            this.SymbolCheckBox.Text = "Symbol";
+            this.SymbolCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // PasswordRichTextBox
+            // 
+            this.PasswordRichTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PasswordRichTextBox.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.PasswordRichTextBox.Location = new System.Drawing.Point(12, 158);
+            this.PasswordRichTextBox.Name = "PasswordRichTextBox";
+            this.PasswordRichTextBox.Size = new System.Drawing.Size(255, 51);
+            this.PasswordRichTextBox.TabIndex = 16;
+            this.PasswordRichTextBox.Text = "";
+            this.PasswordRichTextBox.WordWrap = false;
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.Location = new System.Drawing.Point(116, 48);
+            this.NumberTextBox.MaxLength = 2;
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(19, 19);
+            this.NumberTextBox.TabIndex = 5;
+            this.NumberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.NumberTextBox.TextChanged += new System.EventHandler(this.NumberTextBox_TextChanged);
+            // 
+            // AlphabetUPTextBox
+            // 
+            this.AlphabetUPTextBox.Location = new System.Drawing.Point(116, 70);
+            this.AlphabetUPTextBox.MaxLength = 2;
+            this.AlphabetUPTextBox.Name = "AlphabetUPTextBox";
+            this.AlphabetUPTextBox.Size = new System.Drawing.Size(19, 19);
+            this.AlphabetUPTextBox.TabIndex = 8;
+            this.AlphabetUPTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.AlphabetUPTextBox.TextChanged += new System.EventHandler(this.AlphabetUPTextBox_TextChanged);
+            // 
+            // AlphabetLOWTextBox
+            // 
+            this.AlphabetLOWTextBox.Location = new System.Drawing.Point(116, 92);
+            this.AlphabetLOWTextBox.MaxLength = 2;
+            this.AlphabetLOWTextBox.Name = "AlphabetLOWTextBox";
+            this.AlphabetLOWTextBox.Size = new System.Drawing.Size(19, 19);
+            this.AlphabetLOWTextBox.TabIndex = 11;
+            this.AlphabetLOWTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.AlphabetLOWTextBox.TextChanged += new System.EventHandler(this.AlphabetLOWTextBox_TextChanged);
+            // 
+            // SymbolTextBox
+            // 
+            this.SymbolTextBox.Location = new System.Drawing.Point(116, 114);
+            this.SymbolTextBox.MaxLength = 2;
+            this.SymbolTextBox.Name = "SymbolTextBox";
+            this.SymbolTextBox.Size = new System.Drawing.Size(19, 19);
+            this.SymbolTextBox.TabIndex = 14;
+            this.SymbolTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.SymbolTextBox.TextChanged += new System.EventHandler(this.SymbolTextBox_TextChanged);
+            // 
+            // NumberDataTextBox
+            // 
+            this.NumberDataTextBox.Location = new System.Drawing.Point(141, 48);
+            this.NumberDataTextBox.MaxLength = 10;
+            this.NumberDataTextBox.Name = "NumberDataTextBox";
+            this.NumberDataTextBox.Size = new System.Drawing.Size(126, 19);
+            this.NumberDataTextBox.TabIndex = 6;
+            this.NumberDataTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberDataTextBox_KeyPress);
+            // 
+            // AlphabetUPDataTextBox
+            // 
+            this.AlphabetUPDataTextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AlphabetUPDataTextBox.Location = new System.Drawing.Point(141, 70);
+            this.AlphabetUPDataTextBox.MaxLength = 26;
+            this.AlphabetUPDataTextBox.Name = "AlphabetUPDataTextBox";
+            this.AlphabetUPDataTextBox.Size = new System.Drawing.Size(126, 19);
+            this.AlphabetUPDataTextBox.TabIndex = 9;
+            this.AlphabetUPDataTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AlphabetUPDataTextBox_KeyPress);
+            // 
+            // AlphabetLOWDataTextBox
+            // 
+            this.AlphabetLOWDataTextBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.AlphabetLOWDataTextBox.Location = new System.Drawing.Point(141, 92);
+            this.AlphabetLOWDataTextBox.MaxLength = 26;
+            this.AlphabetLOWDataTextBox.Name = "AlphabetLOWDataTextBox";
+            this.AlphabetLOWDataTextBox.Size = new System.Drawing.Size(126, 19);
+            this.AlphabetLOWDataTextBox.TabIndex = 12;
+            this.AlphabetLOWDataTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.AlphabetLOWDataTextBox_KeyPress);
+            // 
+            // SymbolDataTextBox
+            // 
+            this.SymbolDataTextBox.Location = new System.Drawing.Point(141, 114);
+            this.SymbolDataTextBox.MaxLength = 33;
+            this.SymbolDataTextBox.Name = "SymbolDataTextBox";
+            this.SymbolDataTextBox.Size = new System.Drawing.Size(126, 19);
+            this.SymbolDataTextBox.TabIndex = 15;
+            this.SymbolDataTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SymbolDataTextBox_KeyPress);
+            // 
+            // InitButton
+            // 
+            this.InitButton.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.InitButton.Location = new System.Drawing.Point(189, 11);
+            this.InitButton.Name = "InitButton";
+            this.InitButton.Size = new System.Drawing.Size(47, 34);
+            this.InitButton.TabIndex = 3;
+            this.InitButton.Text = "Init";
+            this.InitButton.UseVisualStyleBackColor = true;
+            this.InitButton.Click += new System.EventHandler(this.InitButton_Click);
+            // 
+            // PasswordCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 221);
+            this.Controls.Add(this.InitButton);
+            this.Controls.Add(this.SymbolDataTextBox);
+            this.Controls.Add(this.AlphabetLOWDataTextBox);
+            this.Controls.Add(this.AlphabetUPDataTextBox);
+            this.Controls.Add(this.NumberDataTextBox);
+            this.Controls.Add(this.SymbolTextBox);
+            this.Controls.Add(this.AlphabetLOWTextBox);
+            this.Controls.Add(this.AlphabetUPTextBox);
+            this.Controls.Add(this.NumberTextBox);
+            this.Controls.Add(this.PasswordRichTextBox);
+            this.Controls.Add(this.SymbolCheckBox);
+            this.Controls.Add(this.AlphabetLOWCheckBox);
+            this.Controls.Add(this.AlphabetUPCheckBox);
+            this.Controls.Add(this.NumberCheckBox);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CRandomButton);
+            this.Controls.Add(this.PasswordLabel);
+            this.Name = "PasswordCreator";
+            this.Text = "PasswordCreator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.Button CRandomButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.CheckBox NumberCheckBox;
+        private System.Windows.Forms.CheckBox AlphabetUPCheckBox;
+        private System.Windows.Forms.CheckBox AlphabetLOWCheckBox;
+        private System.Windows.Forms.CheckBox SymbolCheckBox;
+        private System.Windows.Forms.RichTextBox PasswordRichTextBox;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.TextBox AlphabetUPTextBox;
+        private System.Windows.Forms.TextBox AlphabetLOWTextBox;
+        private System.Windows.Forms.TextBox SymbolTextBox;
+        private System.Windows.Forms.TextBox NumberDataTextBox;
+        private System.Windows.Forms.TextBox AlphabetUPDataTextBox;
+        private System.Windows.Forms.TextBox AlphabetLOWDataTextBox;
+        private System.Windows.Forms.TextBox SymbolDataTextBox;
+        private System.Windows.Forms.Button InitButton;
+    }
+}
+
---
+++ PasswordCreator/PasswordCreator.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PasswordCreator
+{
+    /// <summary>
+    /// パスワード生成
+    /// </summary>
+    public partial class PasswordCreator : Form
+    {
+        /// <summary>
+        /// 数値最大値
+        /// </summary>
+        const int NUMBER_MAX_VALUE = 9;
+
+        /// <summary>
+        /// アルファベット大文字最大値
+        /// </summary>
+        const int ALPHABET_UP_MAX_VALUE = 26;
+
+        /// <summary>
+        /// アルファベット小文字最大値
+        /// </summary>
+        const int ALPHABET_LOW_MAX_VALUE = 26;
+
+        /// <summary>
+        /// 記号最大値
+        /// </summary>
+        const int SYMBOL_MAX_VALUE = 33;
+
+        /// <summary>
+        /// 数値初期値
+        /// </summary>
+        const string NUMBER_INIT_VALUE = "4";
+
+        /// <summary>
+        /// アルファベット大文字初期値
+        /// </summary>
+        const string ALPHABET_UP_INIT_VALUE = "4";
+
+        /// <summary>
+        /// アルファベット小文字初期値
+        /// </summary>
+        const string ALPHABET_LOW_INIT_VALUE = "4";
+
+        /// <summary>
+        /// 記号初期値
+        /// </summary>
+        const string SYMBOL_INIT_VALUE = "4";
+
+        /// <summary>
+        /// 数値データ
+        /// </summary>
+        const string NUMBER_DATA = "7152394608";
+
+        /// <summary>
+        /// アルファベット大文字データ
+        /// </summary>
+        const string ALPHABET_UP_DATA = "XBCSUEPZFYGHIJKAMNOQRTDVWL";
+
+        /// <summary>
+        /// アルファベット小文字データ
+        /// </summary>
+        const string ALPHABET_LOW_DATA = "ybdfgviqxjklmoczprsneahuwt";
+
+        /// <summary>
+        /// 記号データ「! \"#$%&'()*+,-./:;<=>?@[]^_`\\{|}~」
+        /// </summary>
+        const string SYMBOL_DATA = "@%+\\/!#$^?:.(){}~[]-_";
+
+        /// <summary>
+        /// コンストラクタ処理
+        /// </summary>
+        public PasswordCreator()
+        {
+            InitializeComponent();
+
+            NumberCheckBox.Checked = true;
+            AlphabetUPCheckBox.Checked = true;
+            AlphabetLOWCheckBox.Checked = true;
+            SymbolCheckBox.Checked = true;
+            NumberTextBox.Text = NUMBER_INIT_VALUE;
+            AlphabetUPTextBox.Text = ALPHABET_UP_INIT_VALUE;
+            AlphabetLOWTextBox.Text = ALPHABET_LOW_INIT_VALUE;
+            SymbolTextBox.Text = SYMBOL_INIT_VALUE;
+            PasswordRichTextBox.Text = string.Empty;
+            NumberDataTextBox.Text = NUMBER_DATA;
+            AlphabetUPDataTextBox.Text = ALPHABET_UP_DATA;
+            AlphabetLOWDataTextBox.Text = ALPHABET_LOW_DATA;
+            SymbolDataTextBox.Text = SYMBOL_DATA;
+        }
+
+        /// <summary>
+        /// ランダムパスワード生成ボタン押下処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CRandomButton_Click(object sender, EventArgs e)
+        {
+            List<string> passwordList = new List<string>();
+
+            string passwordChars = null;
+            int numberCnt = 0;
+
+            if (NumberCheckBox.Checked && string.Empty != NumberDataTextBox.Text)
+            {
+                passwordChars = NumberDataTextBox.Text;
+
+                int.TryParse(NumberTextBox.Text, out numberCnt);
+
+                passwordList.AddRange(this.GetPassword(passwordChars, numberCnt));
+            }
+
+            if (AlphabetUPCheckBox.Checked && string.Empty != AlphabetUPDataTextBox.Text)
+            {
+                passwordChars = AlphabetUPDataTextBox.Text;
+
+                int.TryParse(AlphabetUPTextBox.Text, out numberCnt);
+
+                passwordList.AddRange(this.GetPassword(passwordChars, numberCnt));
+            }
+
+            if (AlphabetLOWCheckBox.Checked && string.Empty != AlphabetLOWDataTextBox.Text)
+            {
+                passwordChars = AlphabetLOWDataTextBox.Text;
+
+                int.TryParse(AlphabetLOWTextBox.Text, out numberCnt);
+
+                passwordList.AddRange(this.GetPassword(passwordChars, numberCnt));
+            }
+
+            if (SymbolCheckBox.Checked && string.Empty != SymbolDataTextBox.Text)
+            {
+                passwordChars = SymbolDataTextBox.Text;
+
+                int.TryParse(SymbolTextBox.Text, out numberCnt);
+
+                passwordList.AddRange(this.GetPassword(passwordChars, numberCnt));
+            }
+
+            string[] passwords = passwordList.OrderBy(i => Guid.NewGuid()).ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string password in passwords)
+            {
+                sb.Append(password);
+            }
+
+            PasswordRichTextBox.Text = sb.ToString();
+        }
+
+        /// <summary>
+        /// クリアボタン押下処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            NumberCheckBox.Checked = false;
+            AlphabetUPCheckBox.Checked = false;
+            AlphabetLOWCheckBox.Checked = false;
+            SymbolCheckBox.Checked = false;
+            NumberTextBox.Text = string.Empty;
+            AlphabetUPTextBox.Text = string.Empty;
+            AlphabetLOWTextBox.Text = string.Empty;
+            SymbolTextBox.Text = string.Empty;
+            PasswordRichTextBox.Text = string.Empty;
+            NumberDataTextBox.Text = string.Empty;
+            AlphabetUPDataTextBox.Text = string.Empty;
+            AlphabetLOWDataTextBox.Text = string.Empty;
+            SymbolDataTextBox.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// 初期化ボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InitButton_Click(object sender, EventArgs e)
+        {
+            NumberCheckBox.Checked = true;
+            AlphabetUPCheckBox.Checked = true;
+            AlphabetLOWCheckBox.Checked = true;
+            SymbolCheckBox.Checked = true;
+            NumberTextBox.Text = NUMBER_INIT_VALUE;
+            AlphabetUPTextBox.Text = ALPHABET_UP_INIT_VALUE;
+            AlphabetLOWTextBox.Text = ALPHABET_LOW_INIT_VALUE;
+            SymbolTextBox.Text = SYMBOL_INIT_VALUE;
+            PasswordRichTextBox.Text = string.Empty;
+            NumberDataTextBox.Text = NUMBER_DATA;
+            AlphabetUPDataTextBox.Text = ALPHABET_UP_DATA;
+            AlphabetLOWDataTextBox.Text = ALPHABET_LOW_DATA;
+            SymbolDataTextBox.Text = SYMBOL_DATA;
+        }
+
+        /// <summary>
+        /// 数値桁数変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int value = 0;
+
+            if (!int.TryParse(NumberTextBox.Text, out value))
+            {
+                int pos = NumberTextBox.Text.Length - 1;
+
+                if (0 <= pos)
+                {
+                    NumberTextBox.Text = NumberTextBox.Text.Substring(0, pos);
+                }
+            }
+            else
+            {
+                int max = 0;
+
+                int.TryParse(NumberTextBox.Text, out max);
+
+                if (NUMBER_MAX_VALUE < max)
+                {
+                    NumberTextBox.Text = NUMBER_MAX_VALUE.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// アルファベット大文字桁数変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AlphabetUPTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int value = 0;
+
+            if (!int.TryParse(AlphabetUPTextBox.Text, out value))
+            {
+                int pos = AlphabetUPTextBox.Text.Length - 1;
+
+                if (0 <= pos)
+                {
+                    AlphabetUPTextBox.Text = AlphabetUPTextBox.Text.Substring(0, pos);
+                }
+            }
+            else
+            {
+                int max = 0;
+
+                int.TryParse(AlphabetUPTextBox.Text, out max);
+
+                if (ALPHABET_UP_MAX_VALUE < max)
+                {
+                    AlphabetUPTextBox.Text = ALPHABET_UP_MAX_VALUE.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// アルファベット小文字桁数変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AlphabetLOWTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int value = 0;
+
+            if (!int.TryParse(AlphabetLOWTextBox.Text, out value))
+            {
+                int pos = AlphabetLOWTextBox.Text.Length - 1;
+
+                if (0 <= pos)
+                {
+                    AlphabetLOWTextBox.Text = AlphabetLOWTextBox.Text.Substring(0, pos);
+                }
+            }
+            else
+            {
+                int max = 0;
+
+                int.TryParse(AlphabetLOWTextBox.Text, out max);
+
+                if (ALPHABET_LOW_MAX_VALUE < max)
+                {
+                    AlphabetLOWTextBox.Text = ALPHABET_LOW_MAX_VALUE.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 記号桁数変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SymbolTextBox_TextChanged(object sender, EventArgs e)
+        {
+            int value = 0;
+
+            if (!int.TryParse(SymbolTextBox.Text, out value))
+            {
+                int pos = SymbolTextBox.Text.Length - 1;
+
+                if (0 <= pos)
+                {
+                    SymbolTextBox.Text = SymbolTextBox.Text.Substring(0, pos);
+                }
+            }
+            else
+            {
+                int max = 0;
+
+                int.TryParse(SymbolTextBox.Text, out max);
+
+                if (SYMBOL_MAX_VALUE < max)
+                {
+                    SymbolTextBox.Text = SYMBOL_MAX_VALUE.ToString();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 数値データ変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NumberDataTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '0' || '9' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// アルファベット大文字データ変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AlphabetUPDataTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 'A' || 'Z' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// アルファベット小文字データ変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AlphabetLOWDataTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < 'a' || 'z' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// 記号データ変更時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SymbolDataTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar < '!' || '/' < e.KeyChar)
+                && (e.KeyChar < ':' || '@' < e.KeyChar)
+                && (e.KeyChar < '[' || '\'' < e.KeyChar)
+                && (e.KeyChar < '{' || '~' < e.KeyChar) && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// パスワード生成処理
+        /// </summary>
+        /// <param name="passwordChars"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private List<string> GetPassword(string passwordChars, int num)
+        {
+            List<string> resultList = new List<string>();
+
+            Random random = new Random();
+
+            for (int i = 0; i < num; i++)
+            {
+                int pos = random.Next(passwordChars.Length);
+                char c = passwordChars[pos];
+
+                resultList.Add(c.ToString());
+            }
+
+            return resultList;
+        }
+    }
+}
